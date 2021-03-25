6d5778c4b8e0832b086fb10322bc18f115c4ac4b
dhakkan <thatsprinceprofile@gmail.com>
2021-03-26 02:22:57 +0530

    Added Authorization

--- src/main/java/com/pr/app/Application.java
+++ src/main/java/com/pr/app/Application.java
@@ -1,5 +1,6 @@
 package com.pr.app;
 
+import com.pr.app.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +21,10 @@ public class Application {
     @Bean
     public SpringApplicationContext springApplicationContext() {
         return new SpringApplicationContext();
+    }
+
+    @Bean
+    public AppProperties getAppProperties(){
+        return new AppProperties();
     }
 }
